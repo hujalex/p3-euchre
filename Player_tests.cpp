66269e542b471dc4a8be70e2e65fb200eb0e69c8
FyrYT <tobyleng06@gmail.com>
2024-10-09 17:02:33 -0400

    Couldn't finish, have to make meeting, but check out the public test cases and just test the rest of thte functions that i didnt test (you're only testing simple). If you need to just copy the public ones for submission.

--- Player_tests.cpp
+++ Player_tests.cpp
@@ -11,7 +11,60 @@ TEST(test_player_get_name) {
 
     delete alice;
 }
+TEST(test_empty_name)
+{
+    Player* empty = Player_factory("", "Simple");
+    ASSERT_EQUAL(empty->get_name(), "");
+    delete empty;
+}
+TEST(test_add_card) {
+    Player* player = Player_factory("Bob", "Simple");
+    Card c1(ACE, HEARTS);
+    Card c2(KING, SPADES);
+    //c3 and c4 may not work because the card rank is < 9
+    Card c3(TWO, SPADES);
+    Card c4(FIVE, DIAMONDS);
+    player->add_card(c1);
+    player->add_card(c2);
+    player->add_card(c3);
+    player->add_card(c4);
 
-// Add more tests here
+    delete player;
+}
+TEST(test_trump) {
+    Player* player = Player_factory("Alex", "Simple");
+    Card upcard(JACK, HEARTS);
+    Suit order_up_suit = SPADES;
+
+    // Test for round 1 as a non-dealer
+    bool ordered_up = player->make_trump(upcard, false, 1, order_up_suit);
+    if(ordered_up)
+    {
+        ASSERT_EQUAL(ordered_up, true);  
+    }
+    else
+    {
+        ASSERT_EQUAL(ordered_up, false);  
+    }
+
+    // Test for round 1 as a dealer
+    ordered_up = player->make_trump(upcard, true, 1, order_up_suit);
+    if(ordered_up)
+    {
+        ASSERT_EQUAL(ordered_up, true);  
+    }
+    else
+    {
+        ASSERT_EQUAL(ordered_up, false);  
+    }
+    delete player;
+}
+TEST(test_add_and_discard) {
+    Player* player = Player_factory("Toby", "Simple");
+    Card upcard(KING, HEARTS);
+    player->add_and_discard(upcard);
+
+    delete player;
+}
 
 TEST_MAIN()
